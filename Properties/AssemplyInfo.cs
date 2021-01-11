39aac34439a4f5c83a3f3e1810b76661dc7f7aea
wookoa <63658545+wookoa@users.noreply.github.com>
2021-01-11 22:28:44 +0900

    1.1.0.0

--- Properties/AssemplyInfo.cs
+++ Properties/AssemplyInfo.cs
@@ -14,12 +14,8 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //테마별 리소스 사전의 위치
-                                     //(페이지 또는 응용 프로그램 리소스 사진에
-                                     // 리소스가 없는 경우에 사용됨)
-    ResourceDictionaryLocation.SourceAssembly //제네릭 리소스 사전의 위치
-                                              //(페이지 또는 응용 프로그램 리소스 사진에
-                                              // 리소스가 없는 경우에 사용됨)
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
 )]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
